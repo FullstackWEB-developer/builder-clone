46cd5972d2cfaf7f2953015646e7da68db638180
FullstackWEB-developer <sgirad86@gmail.com>
2022-08-20 19:57:16 +0200

    fix: cors

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 import { DragDropContextProvider } from 'react-dnd'
 import HTML5Backend from 'react-dnd-html5-backend'
+import axios from 'axios'
 
 import ModalProvider from 'dcl-dapps/dist/providers/ModalProvider'
 import ToastProvider from 'dcl-dapps/dist/providers/ToastProvider'
@@ -18,6 +19,9 @@ import './modules/analytics/track'
 import './modules/analytics/rollbar'
 import './themes'
 import './index.css'
+
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
 
 ReactDOM.render(
   <Provider store={store}>
